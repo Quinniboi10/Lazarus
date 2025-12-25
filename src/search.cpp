3ea6f7867af5d096098a7853be1e0b5eae2a3674
Quinn <69563433+Quinniboi10@users.noreply.github.com>
2025-12-25 10:08:56 -1000

    Multicut Bench: 3361228

--- src/search.cpp
+++ src/search.cpp
@@ -200,6 +200,8 @@ i16 search(Board& board, i16 depth, const usize ply, i16 alpha, i16 beta, Search
 
             if (score < sBeta)
                 extension = 1;
+            else if (sBeta >= beta)
+                return sBeta;
             // Negative extensions
             else if (ttEntry.score >= beta)
                 extension = -2;
